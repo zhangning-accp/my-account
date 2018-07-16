3497432b13260c59464268c6e5d7604ba6400d60
zhangning_holley <909604945@qq.com>
2018-07-16 14:42:08 +0800

    增加了关闭资源的示例

--- src/main/java/oop/jdbc/JDBCDemo.java
+++ src/main/java/oop/jdbc/JDBCDemo.java
@@ -2,6 +2,7 @@ package oop.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -11,13 +12,6 @@ import java.util.Scanner;
  * 演示通过jdbc 连接数据库和进行增、删、改、查的操作
  */
 public class JDBCDemo {
-    static {
-        System.out.println("JDBCDemo static....");
-    }
-    public JDBCDemo() {
-        System.out.print("JDBCDemo..");
-    }
-
 
     /**
      * 该方法的作用是获取一个数据库连接对象
@@ -42,58 +36,203 @@ public class JDBCDemo {
         return null;
     }
 
-    private void testInsertData(int id,String accountValue,String password) {
+    private void insertData(int id,String accountValue,String password) {
+        Connection connection = null;
+        Statement statement = null;
+
         try {
-            Connection connection = getConnection();
+            connection = getConnection();
             //2. 构建添加数据的sql语句
             String sql = "insert into account " +
                     "values(" + id + ",'" + accountValue + "','" + password + "')";
            //System.out.println(sql);
             //3. 执行sql语句
-            Statement statement = connection.createStatement();
+            statement = connection.createStatement();
             //4. 得到执行后的结果，确定是否添加成功
             int rows = statement.executeUpdate(sql);
             System.out.println("所影响的行数为：" + rows);
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+            if(statement != null) {
+                statement.close();
+            }
+            if(connection != null) {
+                connection.close();
+            }
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    private void testDeleteData(int id) {
+    private void deleteData(int id) {
+        Connection connection = null;
+        Statement statement = null;
         try {
 
-            Connection connection = getConnection();
+            connection = getConnection();
             //2. 构建删除的sql语句
             String sql = "delete from account where id=" + id;
             //3. 执行删除语句
-            Statement statement = connection.createStatement();
+            statement = connection.createStatement();
             //4. 获取执行所影响的行数，判断是否执行成功
             int rows = statement.executeUpdate(sql);
             System.out.println("有" + rows + "行被删除成功！");
 
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if(statement != null) {
+                    statement.close();
+                }
+                if(connection != null) {
+                    connection.close();
+                }
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     private void testUpdateData(int id,String account,String password) {
+        Connection connection = null;
+        Statement statement = null;
+
         try {
 
-            Connection connection = getConnection();
+            connection = getConnection();
             //2. 创建update sql 语句
             String sql = "update account set user_account='" + account +
                     "',user_password='" + password + "' where id=" + id;
             //3. 执行update 语句
-            Statement statement = connection.createStatement();
+            statement = connection.createStatement();
             //4. 得到执行结果，确定是否成功
             int rows = statement.executeUpdate(sql);
             System.out.println("更新结果为：" + (rows > 0));
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if(statement != null) {
+                    statement.close();
+                }
+                if(connection != null) {
+                    connection.close();
+                }
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
+
+    private void findAllData() {
+        //1. 获取数据库连接
+        Connection connection = getConnection();
+        Statement statement = null;
+        ResultSet resultSet = null;
+        //2. 构建查询的sql语句
+        String sql = "select user_account,user_password,id from account";
+        try {
+            //3. 执行sql语句，并获得结果集
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+            //4. 遍历结果集，输出每条记录的信息。
+            StringBuffer buffer = new StringBuffer();
+            buffer.append("------------------------------------------------------------------------------------------------" + System.lineSeparator());
+            buffer.append("id\t\t\taccount\t\t\tpassword\t\t\t" + System.lineSeparator());
+            buffer.append("------------------------------------------------------------------------------------------------" + System.lineSeparator());
+            while(resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String account = resultSet.getString("user_account");
+                String password = resultSet.getString("user_password");
+                buffer.append(id + "\t| " + account + "| \t" + password + "|" + System.lineSeparator());
+            }
+
+            System.out.println(buffer.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(resultSet != null) {
+                    resultSet.close();
+                }
+                if(statement != null) {
+                    statement.close();
+                }
+                if(connection != null) {
+                    connection.close();
+                }
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void findAccountDataById(int id) {
+        //1. 获取数据库连接
+        Connection connection = getConnection();
+        //2. 构建查询的sql语句
+        String sql = "select user_account,user_password,id from account where id=" + id;
+        try {
+            //3. 执行sql语句，并获得结果集
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            //4. 遍历结果集，输出每条记录的信息。
+            StringBuffer buffer = new StringBuffer();
+            buffer.append("------------------------------------------------------------------------------------------------" + System.lineSeparator());
+            buffer.append("id\t\t\taccount\t\t\tpassword\t\t\t" + System.lineSeparator());
+            buffer.append("------------------------------------------------------------------------------------------------" + System.lineSeparator());
+            while(resultSet.next()) {
+                String account = resultSet.getString("user_account");
+                String password = resultSet.getString("user_password");
+                buffer.append(id + "\t| " + account + "| \t" + password + "|" + System.lineSeparator());
+            }
+
+            System.out.println(buffer.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 模糊搜索数据，根据用户用户输入的关键词来模糊查询。
+     * @param keyWord
+     */
+    private void findAccountDataLikeKeyWord(String keyWord) {
+        //1. 获取数据库连接
+        Connection connection = getConnection();
+        //2. 构建查询的sql语句
+        String sql = "select user_account,user_password,id from account " +
+                "where user_account like '%" + keyWord + "%' or user_password like '%" + keyWord + "%'";
+        try {
+            //3. 执行sql语句，并获得结果集
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            //4. 遍历结果集，输出每条记录的信息。
+            StringBuffer buffer = new StringBuffer();
+            buffer.append("------------------------------------------------------------------------------------------------" + System.lineSeparator());
+            buffer.append("id\t\t\taccount\t\t\tpassword\t\t\t" + System.lineSeparator());
+            buffer.append("------------------------------------------------------------------------------------------------" + System.lineSeparator());
+            while(resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String account = resultSet.getString("user_account");
+                String password = resultSet.getString("user_password");
+                buffer.append(id + "\t| " + account + "| \t" + password + "|" + System.lineSeparator());
+            }
+
+            System.out.println(buffer.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
+        JDBCDemo jdbcDemo = new JDBCDemo();
+        //jdbcDemo.findAccountDataLikeKeyWord("5");
         while (true) {
             System.out.println("=============================================================");
             System.out.println("|     欢迎使用HNB 11 人工智能系统  请选择你要进行的操作:   |");
@@ -106,12 +245,12 @@ public class JDBCDemo {
                 select = scanner.nextInt();
             }
             String value = null;
-            JDBCDemo jdbcDemo = new JDBCDemo();
+
             if (select == 1) {//添加数据
                 System.out.println("请输入要添加的账号和密码，中间用逗号分隔.举例：126.com,3456");
                 value = scanner.next();
                 String[] values = value.split(",");
-                jdbcDemo.testInsertData((int) System.currentTimeMillis(),
+                jdbcDemo.insertData((int) System.currentTimeMillis(),
                         values[0], values[1]);
             } else if (select == 2) {// 修改数据
                 System.out.println("请输入要修改的id、账号和密码。逗号分隔。系统将根据id进行数据的更新。id本身不会更新请放心..");
@@ -122,7 +261,7 @@ public class JDBCDemo {
             } else if (select == 3) {// 删除数据
                 System.out.println("请输入要删除的id");
                 value = scanner.next();
-                jdbcDemo.testDeleteData(Integer.parseInt(value));
+                jdbcDemo.deleteData(Integer.parseInt(value));
             } else if (select == 4) {// 退出系统
                 System.exit(-1);
             }
