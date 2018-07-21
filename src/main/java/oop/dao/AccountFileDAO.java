f11d6bce1f92020e6c3d51764b2ee74f7a3ae139
zhangning_holley <909604945@qq.com>
2018-07-21 10:14:22 +0800

    实现了findAll方法

--- src/main/java/oop/dao/AccountFileDAO.java
+++ src/main/java/oop/dao/AccountFileDAO.java
@@ -11,6 +11,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import org.omg.PortableInterceptor.ACTIVE;
 
 /**
  * Created by zn on 2018/7/21.
@@ -18,6 +19,7 @@ import java.util.List;
  * Data access object
  */
 public class AccountFileDAO {
+    private static final String FILE_NAME = "account.hnb";
     public boolean delete(int id) {
         return  false;
     }
@@ -26,32 +28,21 @@ public class AccountFileDAO {
         if(account == null) {
             return false;
         }
-        //读取出文件里所有的数据，然后将新的数据追加到旧的数据上，并整体保存
         StringBuffer buffer = new StringBuffer();
-        try(BufferedReader reader = new BufferedReader(
-                new FileReader("account.hnb"))) {
-                String str = reader.readLine();
-                while(str != null && !str.trim().equals("")) {
-                    buffer.append(str + System.lineSeparator());
-                    str = reader.readLine();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String str = rederFile(FILE_NAME);
+        buffer.append(str);
+
         //1. 选择一个字符文件输出流
-        try(FileWriter writer = new FileWriter("account.hnb");) {
-            //2. 将数据拼接成我们所需要的字符串格式
+        try(FileWriter writer = new FileWriter(FILE_NAME)) {
+            //2. 将数据拼接成我们所需要的字符串格式： id account password
             String data = account.getId() + " " +
                     account.getUserAccount() + " " +
                     account.getUserPassword();
             buffer.append(data + System.lineSeparator());
             writer.write(buffer.toString());//3. 保存到文件
             return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        } catch (IOException e) {e.printStackTrace();}
+
         return false;
     }
 
@@ -59,11 +50,34 @@ public class AccountFileDAO {
         return false;
     }
 
+
+
     public List<Account> findAll(){
-        //1. 创建输入流
-        //2. 读取数据
+        List<Account> list = new ArrayList<>();
+        //1. 创建输入流  //2. 读取数据
+        String str = rederFile(FILE_NAME);
+        String [] rows = str.split(System.lineSeparator());
+        for(String row : rows) {
+            String [] colums = row.split(" ");
+            Account account = new Account();
+            account.setId(Integer.parseInt(colums[0]));
+            account.setUserAccount(colums[1]);
+            account.setUserPassword(colums[2]);
+//            for(int i = 0; i < colums.length; i ++) {
+//                switch (i) {
+//                    case 0: {
+//                        account.setId(Integer.parseInt(colums[0]));
+//                    }case 1:{
+//                        account.setUserAccount(colums[1]);
+//                    }case 2:{
+//                        account.setUserPassword(colums[2]);
+//                    }
+//                }
+//            }
+            list.add(account);
+        }
         //3. 拆分字符串
-        return null;
+        return list;
     }
     public Account findById(int id){
         return null;
@@ -73,12 +87,39 @@ public class AccountFileDAO {
         return null;
     }
 
+    /**
+     * 读取指定的文件，并将内容已String 返回
+     * @param fileName
+     * @return
+     */
+    private String rederFile(String fileName) {
+        //读取出文件里所有的数据，然后将新的数据追加到旧的数据上，并整体保存
+        StringBuffer buffer = new StringBuffer();
+        try(BufferedReader reader = new BufferedReader(
+                new FileReader("account.hnb"))) {
+            // 读取一行数据，io判定一行的标准是看有没有换行符。如果有就是一行
+            // 如果没有则不是。
+            String str = reader.readLine();
+            // 为空的表示没有读取到任何数据，所以多个行之间如果有空行，那么空行后的数据
+            // 将不会被读取。
+            while(str != null && !str.trim().equals("")) {
+                // 拿到当前行数据，并添加一个换行，否则所有的数据都在一行上，不利于后期处理
+                buffer.append(str + System.lineSeparator());
+                str = reader.readLine();// 读取下一行数据
+            }
+        } catch (FileNotFoundException e) {e.printStackTrace();
+        } catch (IOException e) {e.printStackTrace();}
+
+        return buffer.toString();
+    }
+
     public static void main(String [] args) {
         AccountFileDAO dao = new AccountFileDAO();
-        Account account = new Account();
-        account.setId(12);
-        account.setUserAccount("1211.com");
-        account.setUserPassword("sdf11");
-        dao.insert(account);
+//        Account account = new Account();
+//        account.setId(12);
+//        account.setUserAccount("1211.com");
+//        account.setUserPassword("sdf11");
+//        dao.insert(account);
+        dao.findAll();
     }
 }
